489554f8b47a8fa248b0dd2aa96f3e562626dd01
tinalin0 <112659515+tinalin0@users.noreply.github.com>
2022-11-21 14:26:01 -0700

    s

---
+++ js/flashcard-storage-array.js
@@ -0,0 +1,9 @@
+// Loading and Storing of Flashcards
+
+// Public Sets of Flashcards
+let publicArray = [];
+// Name - Terms and Definitions
+
+// Private Sets of Flashcards
+let privateArray = [];
+// Account, Name - Terms and Definitions
