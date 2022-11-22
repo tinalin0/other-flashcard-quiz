9dfbd20dcd15ee76456d4f211b3ddddcdd798998
tinalin0 <112659515+tinalin0@users.noreply.github.com>
2022-11-22 14:35:43 -0700

    Update create-flashcards.js

--- js/create-flashcards.js
+++ js/create-flashcards.js
@@ -8,7 +8,16 @@ function addNewCard() {
 // Helper Functions for Adding Cards
 function createNewDiv(i) {
     let newDiv = document.createElement("div");
+    newDiv.id = "card" + i;
     cardDiv.appendChild(newDiv);
+
+    let termInput = document.createElement("INPUT");
+    termInput.id = "term" + i;
+    termInput.setAttribute("type", "text");
+
+    let defInput = document.createElement("INPUT");
+    defInput.id = "def" + i;
+    defInput.setAttribute("type", "text");
 }
 
 function createDeck() {
