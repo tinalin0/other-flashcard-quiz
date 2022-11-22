20395b4b2e3734ac93a447bdfcdcb7d9fd00c2ae
tinalin0 <112659515+tinalin0@users.noreply.github.com>
2022-11-22 13:58:02 -0700

    s

--- js/main.js
+++ js/main.js
@@ -18,22 +18,6 @@ let signUpLink = document.getElementById("sign-up-link");
 let signInBtn = document.getElementById('sign-in-btn');
 let signUpBtn = document.getElementById('sign-up-btn');
 
-// Home Variables
-
-// Div
-let homeDiv = document.getElementById("home-div");
-
-// Btns = Array
-let homeBtn = document.getElementsByClassName("home-btn");
-
-// Flashcard Variables
-
-// Div
-let flashcardsDiv = document.getElementById("flashcards-div");
-
-// Btns - Array
-let flashcardsBtn = document.getElementsByClassName("flashcards-btn");
-
 // Event Listeners
 
 // SIGN UP BTN CLICKED
@@ -47,7 +31,18 @@ signUpLink.addEventListener("click", displaySignUp);
 
 signInLink.addEventListener("click", displaySignIn);
 
-// Switch Flashcard and Home
+
+// Switch Flashcard and Home 
+
+// Home Variables
+let homeDiv = document.getElementById("home-div");
+let homeBtn = document.getElementsByClassName("home-btn");
+
+// Flashcard Variables
+let flashcardsDiv = document.getElementById("flashcards-div");
+let flashcardsBtn = document.getElementsByClassName("flashcards-btn");
+
+// Event Listeners
 for (let i = 0; i < homeBtn.length; i++) {
     homeBtn[i].addEventListener("click", displayHomeDiv);
 }
@@ -55,3 +50,21 @@ for (let i = 0; i < homeBtn.length; i++) {
 for (let i = 0; i < flashcardsBtn.length; i++) {
     flashcardsBtn[i].addEventListener("click", displayFlashDiv);
 }
+
+// Swtich to Creating Cards
+
+// Variables
+let createDiv = document.getElementById("create-flashcards-div");
+let createDivBtn = document.getElementById("create-btn");
+
+createDivBtn.addEventListener("click", displayCreateDiv);
+
+let newCardBtn = document.getElementById("add-card-btn");
+
+newCardBtn.addEventListener("click", addNewCard);
+
+let createDeckBtn = document.getElementById("create-deck-btn");
+
+createDeckBtn.addEventListener("click", displayHomeDiv);
+createDeckBtn.addEventListener("click", createDeck);
+
