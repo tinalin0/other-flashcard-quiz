bafc30aba4fa04ef4f583f9e7af8fb3746d7571c
tinalin0 <112659515+tinalin0@users.noreply.github.com>
2022-11-24 12:13:53 -0700

    Update create-flashcards.js

--- js/create-flashcards.js
+++ js/create-flashcards.js
@@ -2,25 +2,51 @@ let cardDiv = document.getElementById("new-cards");
 let divArray = [];
 
 function addNewCard() {
-    createNewDiv(divArray.length);
-    cardDiv.innerHTML = divArray;
+    createNewEl(divArray.length);
+    cardDiv.innerHTML = arrayToString(divArray);
 }
 
-// Helper Functions for Adding Cards
-function createNewDiv(i) {
-    let newDiv = document.createElement("div");
-    newDiv.id = "card" + i;
-    cardDiv.appendChild(newDiv);
+// // Helper Functions for Adding Cards
+// function createNewEl(i) {
+//     let newDiv = document.createElement("div");
+//     newDiv.id = "card" + i;
+//     cardDiv.appendChild(newDiv);
 
-    let termInput = document.createElement("INPUT");
-    termInput.id = "term" + i;
-    termInput.setAttribute("type", "text");
+//     let termInput = document.createElement("INPUT");
+//     termInput.id = "term" + i;
+//     termInput.setAttribute("type", "text");
 
-    let defInput = document.createElement("INPUT");
-    defInput.id = "def" + i;
-    defInput.setAttribute("type", "text");
+//     let defInput = document.createElement("INPUT");
+//     defInput.id = "def" + i;
+//     defInput.setAttribute("type", "text");
 
-    divArray.push(newDiv);
+//     divArray.push(newDiv);
+// }
+
+function createNewEl(i) {
+    let divBegin = `<div class="card-div" id="card-${i}>`
+    let divEnd = `</div>`
+    let inTerm = `<input type="text" id="term-${i}" placeholder="Term">`
+    let inDef = `<input type="text" id="def-${i}" placeholder="Definition">`
+    divArray.push(createNewCard(divBegin, divEnd, inTerm, inDef));
+}
+
+function createNewCard(divStart, divEnd, term, def) {
+    return {
+        div: divStart + divEnd,
+        divStart: divStart,
+        divEnd: divEnd,
+        term: term,
+        def: def,
+    };
+}
+
+function arrayToString() {
+    let arrayString = "";
+    for (let i = 0; i < divArray.length; i++) {
+        arrayString = arrayString + divArray.divStart + divArray.term + divArray.def + divArray.divEnd;
+    }
+    return arrayString;
 }
 
 function createDeck() {
