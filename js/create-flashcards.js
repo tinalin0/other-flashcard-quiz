776b7112d72bb1f3ed5efbb367828297e862df7c
tinalin0 <112659515+tinalin0@users.noreply.github.com>
2022-11-25 14:32:42 -0700

    Update create-flashcards.js

--- js/create-flashcards.js
+++ js/create-flashcards.js
@@ -2,52 +2,62 @@ let cardDiv = document.getElementById("new-cards");
 let divArray = [];
 
 function addNewCard() {
+    // update divArray
     createNewEl(divArray.length);
-    cardDiv.innerHTML = arrayToString(divArray);
+    divArray.push(updateDiv(divArray.length, ));
 }
 
-// // Helper Functions for Adding Cards
-// function createNewEl(i) {
-//     let newDiv = document.createElement("div");
-//     newDiv.id = "card" + i;
-//     cardDiv.appendChild(newDiv);
-
-//     let termInput = document.createElement("INPUT");
-//     termInput.id = "term" + i;
-//     termInput.setAttribute("type", "text");
-
-//     let defInput = document.createElement("INPUT");
-//     defInput.id = "def" + i;
-//     defInput.setAttribute("type", "text");
-
-//     divArray.push(newDiv);
-// }
-
+// Helper Functions for Adding Cards
 function createNewEl(i) {
-    let divBegin = `<div class="card-div" id="card-${i}>`
-    let divEnd = `</div>`
-    let inTerm = `<input type="text" id="term-${i}" placeholder="Term">`
-    let inDef = `<input type="text" id="def-${i}" placeholder="Definition">`
-    divArray.push(createNewCard(divBegin, divEnd, inTerm, inDef));
+    let newDiv = document.createElement("div");
+    newDiv.id = "card" + i;
+
+    let termInput = document.createElement("INPUT");
+    termInput.id = "term" + i;
+    termInput.setAttribute("type", "text");
+    termInput.setAttribute("placeholder", "Term " + i)
+
+    let defInput = document.createElement("INPUT");
+    defInput.id = "def" + i;
+    defInput.setAttribute("type", "text");
+    defInput.setAttribute("placeholder", "Definition")
+
+    createNewCard(newDiv, termInput, defInput)
 }
 
-function createNewCard(divStart, divEnd, term, def) {
+function createNewCard(div, term, def) {
+    cardDiv.appendChild(div);
+    div.appendChild(term);
+    div.appendChild(def);
+}
+
+function updateDiv(i, term, def) {
     return {
-        div: divStart + divEnd,
-        divStart: divStart,
-        divEnd: divEnd,
+        place: i,
         term: term,
         def: def,
     };
 }
 
-function arrayToString() {
-    let arrayString = "";
-    for (let i = 0; i < divArray.length; i++) {
-        arrayString = arrayString + divArray.divStart + divArray.term + divArray.def + divArray.divEnd;
-    }
-    return arrayString;
+function loopToLast() {
+    
 }
+
+// function createNewEl(i) {
+//     let divBegin = `<div class="card-div" id="card-${i}>`
+//     let divEnd = `</div>`
+//     let inTerm = `<input type="text" id="term-${i}" placeholder="Term">`
+//     let inDef = `<input type="text" id="def-${i}" placeholder="Definition">`
+//     divArray.push(createNewCard(divBegin, divEnd, inTerm, inDef));
+// }
+
+// function arrayToString() {
+//     let arrayString = "";
+//     for (let i = 0; i < divArray.length; i++) {
+//         arrayString = arrayString + divArray.divStart + divArray.term + divArray.def + divArray.divEnd;
+//     }
+//     return arrayString;
+// }
 
 function createDeck() {
 
